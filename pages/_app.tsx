c8131bc55819166a337478bde0f887bdde1ef779
Teerth Patel <pteerth989@gmail.com>
2023-10-05 12:04:09 +0530

    added authentication with graphql

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Inter } from "next/font/google";
+import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
-      <Component {...pageProps} />
+      <GoogleOAuthProvider clientId="62366647663-573no4br5r6l08rul7es554ghpfd8fu9.apps.googleusercontent.com">
+        <Component {...pageProps} />
+        <Toaster />
+      </GoogleOAuthProvider>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import { FaXTwitter } from "react-icons/fa6";
 import { RiHome7Fill } from "react-icons/ri";
-import { BiSearch } from "react-icons/bi";
+import { BiSearch, BiEnvelope } from "react-icons/bi";
 import { GrNotification } from "react-icons/gr";
-import { BiEnvelope } from "react-icons/bi";
 import { BsBookmark } from "react-icons/bs";
 import { HiOutlineUser } from "react-icons/hi";
 import { CiCircleMore } from "react-icons/ci";
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google'
 import FeedCard from '@/components/FeedCard'
 
-import React from "react";
+import React, { useCallback } from "react";
+import toast from "react-hot-toast";
+import { graphqlClient } from "@/clients/api";
+import { verifyUserGoogleTokenQuery } from "@/graphql/queries/user";
 
 interface XSiderbarButton {
   title: string;
@@ -52,6 +55,19 @@ const SidbarMenuItems: XSiderbarButton[] = [
 ];
 
 export default function Home() {
+  const handleLoginWithGoogle = useCallback(async (cred: CredentialResponse) => {
+    const googleToken = cred.credential
+    if (!googleToken) return toast.error(`Google token not found`);
+
+    const { verifyGoogleToken } = await graphqlClient.request(verifyUserGoogleTokenQuery, { token: googleToken });
+
+    toast.success("Verified Sucess!")
+
+    if (verifyGoogleToken) {
+      window.localStorage.setItem('twitter_token', verifyGoogleToken)
+    }
+    console.log(verifyGoogleToken)
+  }, [])
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen px-56 bg-white">
@@ -88,7 +104,12 @@ export default function Home() {
           <FeedCard />
           <FeedCard />
         </div>
-        <div className="col-span-3"></div>
+        <div className="col-span-3">
+          <div className="p-5 bg-[#E7E7E8]">
+            <h1 className="my-2 text-2xl"> New to X-twitter?</h1>
+            <GoogleLogin onSuccess={handleLoginWithGoogle} />
+          </div>
+        </div>
       </div>
     </div>
   );
